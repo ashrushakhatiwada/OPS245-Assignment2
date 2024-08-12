430dfe3373b44f24ccb90b322a5f0e2d7d5d4d89
ashrusha <akhatiwada@myseneca.ca>
2024-08-11 23:44:51 -0400

    Final Submission with all check-pass

--- assignment2.py
+++ assignment2.py
@@ -37,25 +37,64 @@ def parse_command_args() -> object:
 # -r running only
 
 def percent_to_graph(percent: float, length: int=20) -> str:
-    "turns a percent 0.0 - 1.0 into a bar graph"
+    """turns a percent 0.0 - 1.0 into a bar graph
+    making a graph with ###"""
     ...
-# percent to graph function
+    num_hashes = int(percent * length)
+    num_spaces = length - num_hashes
+    return '#' * num_hashes + ' ' * num_spaces
 
 def get_sys_mem() -> int:
-    "return total system memory (used or available) in kB"
-    ...
+    """return total system memory (used or available) in kB
+    get total sys mem"""
+    file_path = '/proc/meminfo' 
+    try:
+        with open(file_path, 'r') as file:  #open file
+            for entry in file:  # read all line
+                if 'MemTotal:' in entry: #look for MemTotal in lines
+                    memory_total = int(entry.split()[1])
+                    return memory_total  
+    except: #except error
+        return 0 #return 0 if error
 
 def get_avail_mem() -> int:
-    "return total memory that is available"
-    ...
+    """return total memory that is available
+    get available mem"""
+    file_path = '/proc/meminfo'
+    try:
+        mem_file = open(file_path, 'r') #open file
+        lines = mem_file.readlines() # read line
+        mem_file.close() # close file
+        for line in lines: #look for memtotal line in each line
+            if 'MemAvailable:' in line:
+                available_mem = int(line.split()[1])
+                return available_mem
+    except : #ecpect error and return 0 if error
+        return 0
 
 def pids_of_prog(app_name: str) -> list:
     "given an app name, return all pids associated with app"
-    ...
+    try:
+        pid_command = f'pidof {app_name}' #get pids command
+        pid_output = os.popen(pid_command).read().strip() #runing command reading output
+        if pid_output: #if output
+            pid_list = pid_output.split() #split the output
+        else:
+            pid_list = [] #if no output empty list
+        return pid_list
+    except: #if error
+        print(f"Error getting PIDs for {app_name}")
+        return []
 
 def rss_mem_of_pid(proc_id: str) -> int:
     "given a process id, return the resident memory used, zero if not found"
-    ...
+    try:
+        f = open(f'/proc/{proc_id}/status', 'r') #open the status file
+        for line in f: #real line
+                if 'VmRSS:' in line: #look for VmRSS line
+                    return int(line.split()[1])  
+    except ValueError: #if error
+        return 0
 
 def bytes_to_human_r(kibibytes: int, decimal_places: int=2) -> str:
     "turn 1,024 into 1 MiB, for example"
@@ -71,10 +110,57 @@ def bytes_to_human_r(kibibytes: int, decimal_places: int=2) -> str:
 
 if __name__ == "__main__":
     args = parse_command_args()
-    if not args.program:
-        ...
-    else:
-        ...
+    
+    total_mem = get_sys_mem()
+    avail_mem = get_avail_mem()
+    used_mem = total_mem - avail_mem
+    used_percent = used_mem / total_mem
+    
+    if not args.program: #if no program
+        graph = percent_to_graph(used_percent, args.length) #make graph
+        percentage = int(used_percent * 100)
+        if args.human_readable: #if human readable is asked
+            used_mem_str = bytes_to_human_r(used_mem)#convert to human readable
+            total_mem_str = bytes_to_human_r(total_mem)
+        else:
+            used_mem_str = str(used_mem) #keep default
+            total_mem_str = str(total_mem) #keep default
+        
+        print(f"Memory         [{graph} | {percentage}%] {used_mem_str}/{total_mem_str}")
+    else: #if app specified
+        pid_list = pids_of_prog(args.program) #get list of pids
+        if not pid_list: #if not found
+            print(f"{args.program} not found.")
+        
+        total_program_mem = 0 #initialize total program mem
+        for pid in pid_list: 
+            pid_mem = rss_mem_of_pid(pid)
+            total_program_mem += pid_mem
+            pid_mem_percent = pid_mem / total_mem #calculate percentage
+            graph = percent_to_graph(pid_mem_percent, args.length) #make bar graph
+            pid_percentage = int(pid_mem_percent * 100)
+            
+            if args.human_readable: #if human readable needed
+                pid_mem_str = bytes_to_human_r(pid_mem) #convert to human readable
+                total_mem_str = bytes_to_human_r(total_mem)
+            else:
+                pid_mem_str = str(pid_mem)
+                total_mem_str = str(total_mem)
+                
+            print(f"{pid}         [{graph} | {pid_percentage}%] {pid_mem_str}/{total_mem_str}")
+        
+        total_program_percent = total_program_mem / total_mem
+        graph = percent_to_graph(total_program_percent, args.length)
+        program_percentage = int(total_program_percent * 100)
+        
+        if args.human_readable:
+            total_program_mem_str = bytes_to_human_r(total_program_mem)
+            total_mem_str = bytes_to_human_r(total_mem)
+        else:
+            total_program_mem_str = str(total_program_mem)
+            total_mem_str = str(total_mem)
+        
+        print(f"{args.program}        [{graph} | {program_percentage}%] {total_program_mem_str}/{total_mem_str}")
     # process args
     # if no parameter passed, 
     # open meminfo.
